c110bae268aa9c43a74e826efbb69b7e75507654
mohamor2002 <muhammadamor2002@gmail.com>
2023-10-09 00:10:41 +0100

    questions-2.0

--- src/constants/Quests.js
+++ src/constants/Quests.js
@@ -1,67 +1,131 @@
 
 export const Quests =[
     {
-        question:'taybt kharya w zadtlha chwi formaj w mlh, kifach reaction ta3k?',
+        question:'Rak f l ESI w 3gb 3lik prof, wch dir ?',
         choices:[
             {
-                choice:'Overrated .',
-                characters:['fadi']
+                choice:'Ndir nech w smir',
+                characters:['Sdide9']
             },
             {
-                choice:'Lmould 7ram wiwiwiwiwi!',
-                characters:['ws']
+                choice:'Ngolo saha chikh w maydihach fiya',
+                characters:['Fadel']
             },
             {
-                choice:'zayr rohk',
-                characters:['zoubir']
+                choice:'Normal nkml wch rani ndir',
+                characters:['Lws','Zoubir','Attia','Raouf','Lgr3ich','Kader','Fadi','L3mdch']
             },
             {
-                choice:'بأبي و أمي , الأكل .. الجنس ...',
-                characters:['3mor']
+                choice:'ESI ? mzalni fel frach wchmn prof 3aychk',
+                characters:['3mor','Tobni']
             }
         ]
     },
     {
-        question:'taybt kharya w zadtlha chwi formaj w mlh, kifach reaction ta3k?',
+        question:'LCous fatek, wch dir ?',
         choices:[
             {
-                choice:'Overrated .',
-                characters:['fadi']
+                choice:'A 3morra3!! aw kayn cousaaa!?',
+                characters:['Kader']
             },
             {
-                choice:'Lmould 7ram wiwiwiwiwi!',
-                characters:['ws']
+                choice:'Sahla! na7kem taxi mn sidhom mbarek (:',
+                characters:['Lws','3mor','Lgr3ich','Attia','Raouf','Tobni','L3mdch']
             },
             {
-                choice:'zayr rohk',
-                characters:['zoubir']
+                choice:'HHHHHHHHHH Yroho y9raw fel Cous msakn',
+                characters:['Fadi','Zoubir']
             },
             {
-                choice:'بأبي و أمي , الأكل .. الجنس ...',
-                characters:['3mor']
+                choice:'Nji f stop bayna',
+                characters:['Fadel','Sdide9']
             },
             
         ]
     },
     {
-        question:'taybt kharya w zadtlha chwi formaj w mlh, kifach reaction ta3k?',
+        question:'Ghodwa Interro w marak 9ari walou, kifah derna?',
         choices:[
             {
-                choice:'Overrated .',
-                characters:['fadi']
+                choice:'Lmad comdab, freeman ...',
+                characters:['3mor','Fadel']
             },
             {
-                choice:'Lmould 7ram wiwiwiwiwi!',
-                characters:['ws']
+                choice:'Nrigliha bou5fif w norgod',
+                characters:['Lws','Kader','Zoubir','Lgr3ich','Fadi']
             },
             {
-                choice:'zayr rohk',
-                characters:['zoubir']
+                choice:'Na3tiha b rgda wchmn interro 3aychk !',
+                characters:['Tobni','Sdide9']
             },
             {
-                choice:'بأبي و أمي , الأكل .. الجنس ...',
-                characters:['3mor']
+                choice:'Nta mridh ana mnich 9ari ?!',
+                characters:['Raouf','Attia','L3mdch']
             }
         ]
-    }
+    },
+    {
+        question:'Jo3t 3la 12:00 te3 lil, wch dir ?',
+        choices:[
+            {
+                choice:'Samir yfriha (gatou kawkaw kesra ...)',
+                characters:['Lws','Kader','Fadi','L3mdch','Tobni']
+            },
+            {
+                choice:'Ma9arouna > Football > Abi wa Omi',
+                characters:['3mor','Fadel','Sdide9']
+            },
+            {
+                choice:'Hhhhhh wchnhi 12:00 t3 lil hadi?',
+                characters:['Raouf','Lgr3ich','Attia']
+            },
+            {
+                choice:'Aaaah resto saker norgod khirli !',
+                characters:['Zoubir']
+            }
+        ]
+    },
+    {
+        question:'Ljma3a galolk fiha kharja, wch dir?',
+        choices:[
+            {
+                choice:'Nji fiha',
+                characters:['Attia','3mor','Fadel','Raouf','Zoubir','Tobni']
+            },
+            {
+                choice:'Ana li naddemt l5arja!',
+                characters:['Lgr3ich','Lws','Kader','L3mdch']
+            },
+            {
+                choice:'Fiha, bs7 alger centre overrated !',
+                characters:['Fadi']
+            },
+            {
+                choice:'Kadtni mnjich',
+                characters:['Sdide9']
+            }
+        ]
+    },
+    {
+        question:'Wch t3lmt f projet 2cp ?',
+        choices:[
+            {
+                choice:'Walou hhhh',
+                characters:['Tobni','Fadel','Fadi']
+            },
+            {
+                choice:'Kifach tdir sada9at (;',
+                characters:['Lws','Kader','Sdide9']
+            },
+            {
+                choice:'Kifach tgere equipe te3 mhabel',
+                characters:['Zoubir','3mor','L3mdch']
+            },
+            {
+                choice:'T3lmt T3lmt ...',
+                characters:['Attia','Lgr3ich','Raouf']
+            }
+        ]
+    },
+    
 ]
